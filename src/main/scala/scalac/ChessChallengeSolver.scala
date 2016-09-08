5f57920bda42cbedcc420cd1421be8e969eb76c4
Ignacio Mosca <ignaciomosca@gmail.com>
2016-09-08 08:09:31 -0300

    Add findCandidate

--- src/main/scala/scalac/ChessChallengeSolver.scala
+++ src/main/scala/scalac/ChessChallengeSolver.scala
@@ -5,28 +5,32 @@ package scalac
   */
 object ChessChallengeSolver {
 
+  def findCandidate(chessPiece: ChessPiece, board: Board): ChessPiece = {
+    val candidates = for {
+      r <- 1 to board.getM
+      c <- 1 to board.getN
+      p = ChessPieceUtils.createPiece(chessPiece, r, c)
+      if(board.isSafe(p))
+    } yield p
+    candidates.head
+  }
+
   def solution(board: Board, pieces: List[ChessPiece], solutions: Set[Board], testedConfigurations: Set[Board]): Set[Board] = {
     if (!pieces.isEmpty) {
-      val candidates: List[ChessPiece] = for {
-        r <- 1 to board.getM
-        c <- 1 to board.getN
-        p <- ChessPieceUtils.createPiece(pieces.head, r, c)
-        if (board.isSafe(p))
-      } yield p
-
-      val candidate = candidates.head
+      val candidate = findCandidate(pieces.head, board)
       val b = board.place(candidate)
       if (pieces.size != 1) {
-        if(!testedConfigurations.contains(b)){
-          solution(b,pieces.tail,solutions,testedConfigurations+b)
+        if (!testedConfigurations.contains(b)) {
+          solution(b, pieces.tail, solutions, testedConfigurations + b)
         }
-      }else{
-        if(!solutions.contains(b)){
-          solution(b,pieces.tail,solutions+b,testedConfigurations)
+      } else {
+        if (!solutions.contains(b)) {
+          solution(b, pieces.tail, solutions + b, testedConfigurations)
         }
       }
     }
     solutions
   }
+
 }
 
