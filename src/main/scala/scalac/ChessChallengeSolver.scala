e0f314dc2693a296b698d86d5971e7051bc6714b
Ignacio Mosca <ignaciomosca@gmail.com>
2016-09-09 08:51:20 -0300

    Add documentation Still having issues with repeated solutions

--- src/main/scala/scalac/ChessChallengeSolver.scala
+++ src/main/scala/scalac/ChessChallengeSolver.scala
@@ -5,25 +5,41 @@ package scalac
   */
 object ChessChallengeSolver {
 
+  /** *
+  * @param board                ChessBoard
+  * @param pieces               Chess Pieces selected by the user
+  * @param solutions            valid solutions to the problem
+  * @param testedConfigurations Board configurations to which solutions were not found
+  * @return return a list of possible solutions to the problem in the form of a list of filled chess boards
+  */
   def solution(board: Board, pieces: List[ChessPiece], solutions: Set[Board], testedConfigurations: Set[Board]): Set[Board] = {
     if (!pieces.isEmpty) {
-      val candidateBoard = findCandidatePosition(pieces.head, board, testedConfigurations)
+      val candidateBoard = findCandidatePosition(pieces.head, board, solutions, testedConfigurations)
       val setBoards = (for{
         b <- candidateBoard
-      } yield solution(b, pieces.tail, Set(), Set())).foldLeft(Set[Board]())((s,b) => s++b)
+      } yield solution(b, pieces.tail, solutions+b, testedConfigurations+b)).foldLeft(Set[Board]())((s,b) => s++b)
       setBoards
     } else {
+      board.show()
       Set(board)
     }
   }
 
-  private def findCandidatePosition(chessPiece: ChessPiece, board: Board, testedConfigurations: Set[Board]) = {
+  /***
+    *
+    * @param chessPiece piece to be placed
+    * @param board board in which to place the piece
+    * @param solutions solutions that were already found
+    * @param testedConfigurations configurations that were already tested
+    * @return returns a possible board to add to the solutions Set
+    */
+  private def findCandidatePosition(chessPiece: ChessPiece, board: Board, solutions: Set[Board], testedConfigurations: Set[Board]) = {
     for {
       r <- 1 to board.getM
       c <- 1 to board.getN
       p = ChessPieceUtils.createPiece(chessPiece, r, c)
       b = board.place(p)
-      if (board.isSafe(p) && !testedConfigurations.contains(b))
+      if (board.isSafe(p) && !testedConfigurations.contains(b) && !solutions.contains(b))
     } yield b
   }
 
