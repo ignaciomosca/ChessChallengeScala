1d105205b7607c095903f06f8e9fe363c96a6e06
Ignacio Mosca <ignacio.mosca@customink.com>
2016-09-09 17:33:30 -0300

    Add equals and hashCode to remove duplicates

--- src/main/scala/scalac/Board.scala
+++ src/main/scala/scalac/Board.scala
@@ -3,14 +3,15 @@ package scalac
 /**
   * Created by ignacio on 05/09/16.
   */
-class Board(M: Int, N: Int, usedPieces: List[ChessPiece]) {
+class Board(M: Int, N: Int, usedPieces: Set[ChessPiece]) {
 
   def getM = M
   def getN = N
+  def getUsedPieces = usedPieces;
 
   def isSafe(c: ChessPiece): Boolean = usedPieces.forall(p => !c.attacks(p) && !p.attacks(c))
 
-  def place(c: ChessPiece) = new Board(M, N, c :: usedPieces)
+  def place(c: ChessPiece) = new Board(M, N, usedPieces+c)
 
   def findChessPiece(row: Int, col: Int):Char = usedPieces.find(p => p.row==row && p.col==col).map(p => p.piece()).getOrElse('_')
 
@@ -22,6 +23,18 @@ class Board(M: Int, N: Int, usedPieces: List[ChessPiece]) {
       println()
     }
     println()
+  }
+
+  override def equals(that: Any): Boolean =
+    that match {
+      case that: Board => that.getUsedPieces.equals(this.getUsedPieces)
+      case _ => false
+    }
+  override def hashCode: Int = {
+    val prime = 31
+    var result = 1
+    result = prime * result + this.getUsedPieces.hashCode();
+    return result
   }
 
 }
