8f4d2ef1faa80a73442151e0a0a3b396659b597b
Ignacio Mosca <ignacio.mosca@customink.com>
2016-09-19 18:03:08 -0300

    Tidy up

--- src/main/scala/scalac/ChessChallengeSolver.scala
+++ src/main/scala/scalac/ChessChallengeSolver.scala
@@ -12,19 +12,15 @@ object ChessChallengeSolver {
     * @param solutions valid solutions to the problem
     * @return return a list of possible solutions to the problem in the form of a list of filled chess boards
     */
-  def solution(board: Board, pieces: List[ChessPiece], solutions: Set[Board]): Set[Board] = {
-    if (!pieces.isEmpty) {
+  def solution(board: Board, pieces: List[ChessPiece], solutions: Set[Board]): Set[Board] = pieces match {
+    case Nil => Set(board)
+    case piece::ps =>
       (for {
         r <- 1 to board.M
         c <- 1 to board.N
-        p = ChessPieceUtils.createPiece(pieces.head, r, c)
+        p = ChessPieceUtils.createPiece(piece, r, c)
         b = board.place(p)
         if (board.isSafe(p) && !solutions.contains(b))
-      } yield solution(b, pieces.tail, solutions + b)).foldLeft(Set[Board]())((s, b) => s ++ b)
-    } else {
-      Set(board)
-    }
+      } yield solution(b, ps, solutions + b)).foldLeft(Set[Board]())((s, b) => s ++ b)
   }
 }
-
-
