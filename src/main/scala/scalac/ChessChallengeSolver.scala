ce6cd9063e315e87732ab91f687ff76b227bf845
Ignacio Mosca <ignaciomosca@gmail.com>
2016-09-22 00:04:06 -0300

    Fix with tail recursion, benchmarks and other suggestions

--- src/main/scala/scalac/ChessChallengeSolver.scala
+++ src/main/scala/scalac/ChessChallengeSolver.scala
@@ -1,5 +1,7 @@
 package scalac
 
+import scala.annotation.tailrec
+
 /**
   * Created by ignaciomosca on 9/6/16.
   */
@@ -7,20 +9,61 @@ object ChessChallengeSolver {
 
   /**
     *
-    * @param board     ChessBoard
-    * @param pieces    Chess Pieces selected by the user
-    * @param solutions valid solutions to the problem
+    * @param board  initial Board with no pieces placed
+    * @param pieces Chess Pieces selected by the user to be placed
     * @return return a list of possible solutions to the problem in the form of a list of filled chess boards
     */
-  def solution(board: Board, pieces: List[ChessPiece], solutions: Set[Board]): Set[Board] = pieces match {
-    case Nil => Set(board)
-    case piece::ps =>
-      (for {
-        r <- 1 to board.M
-        c <- 1 to board.N
-        p = ChessPieceUtils.createPiece(piece, r, c)
-        b = board.place(p)
-        if (board.isSafe(p) && !solutions.contains(b))
-      } yield solution(b, ps, solutions + b)).foldLeft(Set[Board]())((s, b) => s ++ b)
+  def solution(board: Board, pieces: List[ChessPiece]): Set[Board] = obtainSolution(pieces, Set(board)).filter(b=>b.done)
+
+  /**
+    *
+    * @param pieces    remaining pieces to be placed
+    * @param solutions list of solutions
+    * @return obtains a new solution as long as there are pieces to be placed, if there are not it returns the entire set
+    */
+  @tailrec
+  private def obtainSolution(pieces: List[ChessPiece], solutions: Set[Board]): Set[Board] = pieces match {
+    case p :: ps => obtainSolution(ps, candidateSolution(p, solutions, Set[Board]()))
+    case Nil => solutions
   }
+
+  /** *
+    *
+    * @param candidatePiece  piece to be placed
+    * @param candidateBoards board in which it will be placed
+    * @param solutions       list of current solutions
+    * @return find candidate boards with the candidatePiece placed
+    */
+  private def candidateSolution(candidatePiece: ChessPiece, candidateBoards: Set[Board], solutions: Set[Board]): Set[Board] = {
+    for{
+      board <- candidateBoards
+      c <- findCandidateSolution(candidatePiece, board)
+    }yield c
+  }
+
+  /** *
+    *
+    * @param candidatePiece possible piece to be placed
+    * @param board          board in which it will be placed
+    * @return iterate over all possible positions and look for a position in which a new board configuration can be created.
+    *         If it's a solution add it to solutions list
+    */
+  private def findCandidateSolution(candidatePiece: ChessPiece, board: Board): Set[Board] = {
+    def findCandidateSolutionAux(usedPieces: Set[ChessPiece], solutions: Set[Board]): Set[Board] = {
+      if (usedPieces.size != board.initialPieces) {
+        (for {
+          r <- 1 to board.M
+          c <- 1 to board.N
+          p = ChessPieceUtils.createPiece(candidatePiece, r, c)
+          b = board.place(p)
+          if (board.isSafe(p) && !solutions.contains(b))
+        } yield b) (collection.breakOut)
+      } else {
+        solutions
+      }
+    }
+    findCandidateSolutionAux(board.usedPieces, Set[Board]())
+  }
+
+
 }
--- src/test/scala/ChessChallengeSolverTest.scala
+++ src/test/scala/ChessChallengeSolverTest.scala
@@ -12,19 +12,36 @@ import scalac._
 class ChessChallengeSolverTest extends FunSuite {
 
   test("3x3 board containing 2 Kings and 1 Rook") {
-    val solutions = ChessChallengeSolver.solution(new Board(3, 3, Set(),3), List(King(0, 0), King(0, 0), Rook(0, 0)), Set())
+    val start = System.currentTimeMillis()
+    val solutions = ChessChallengeSolver.solution(new Board(3, 3, Set(),3), List(King(0, 0), King(0, 0), Rook(0, 0)))
+    val finish = System.currentTimeMillis()
     solutions.foreach(b => b.show())
+    println(s"Elapsed time: ${finish-start} ms")
     assert(solutions.size == 4)
   }
   test("4x4 board containing 3 Rooks and 4 Knighs") {
-    val solutions = ChessChallengeSolver.solution(new Board(4, 4, Set(),6), List(Knight(0, 0), Knight(0, 0), Knight(0, 0), Knight(0, 0), Rook(0, 0), Rook(0, 0)), Set())
+    val start = System.currentTimeMillis()
+    val solutions = ChessChallengeSolver.solution(new Board(4, 4, Set(),6), List(Knight(0, 0), Knight(0, 0), Knight(0, 0), Knight(0, 0), Rook(0, 0), Rook(0, 0)))
+    val finish = System.currentTimeMillis()
     solutions.foreach(b => b.show())
+    println(s"Elapsed time: ${finish-start} ms")
     assert(solutions.size == 8)
   }
-  test("8x8 board returns 92 solutions") {
-    val solutions = ChessChallengeSolver.solution(new Board(8, 8, Set(),8), List(Queen(0, 0), Queen(0, 0), Queen(0, 0), Queen(0, 0), Queen(0, 0), Queen(0, 0), Queen(0, 0), Queen(0, 0)), Set())
+  test("8x8 board with 8 Queens returns 92 solutions") {
+    val start = System.currentTimeMillis()
+    val solutions = ChessChallengeSolver.solution(new Board(8, 8, Set(),8), List(Queen(0, 0), Queen(0, 0), Queen(0, 0), Queen(0, 0), Queen(0, 0), Queen(0, 0), Queen(0, 0), Queen(0, 0)))
+    val finish = System.currentTimeMillis()
     solutions.take(5).foreach(b => b.show())
+    println(s"Elapsed time: ${finish-start} ms")
     assert(solutions.size == 92)
+  }
+  test("7x7 board with 2 Kings, 2 Queens, 2 Bishops and a Knight returns 3063828 solutions") {
+    val start = System.currentTimeMillis()
+    val solutions = ChessChallengeSolver.solution(new Board(7, 7, Set(), 7), List(King(0, 0), King(0, 0), Queen(0, 0), Queen(0, 0), Bishop(0,0), Bishop(0,0), Knight(0,0)))
+    val finish = System.currentTimeMillis()
+    solutions.take(5).foreach(b => b.show())
+    println(s"Elapsed time: ${finish-start} ms")
+    assert(solutions.size == 3063828)
   }
 
 }
