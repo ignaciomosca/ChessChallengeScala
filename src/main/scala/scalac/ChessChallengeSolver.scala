1e8dff960b12092a318449c84e91c776fa74b5ad
Ignacio Mosca <ignaciomosca@gmail.com>
2016-10-08 11:32:19 -0300

    Remove unneccessary Aux pattern findCandidateSolutionAux

--- src/main/scala/scalac/ChessChallengeSolver.scala
+++ src/main/scala/scalac/ChessChallengeSolver.scala
@@ -15,7 +15,6 @@ object ChessChallengeSolver {
     */
   def solution(board: Board, pieces: List[ChessPiece]): Set[Board] = obtainSolution(pieces, Set(board)).filter(_.done)
 
-
   @tailrec
   private def obtainSolution(pieces: List[ChessPiece], solutions: Set[Board]): Set[Board] = pieces match {
     case p :: ps => obtainSolution(ps, candidateSolution(p, solutions))
@@ -23,35 +22,26 @@ object ChessChallengeSolver {
   }
 
   private def candidateSolution(candidatePiece: ChessPiece, candidateBoards: Set[Board]): Set[Board] = {
-    for{
+    for {
       board <- candidateBoards
       c <- findCandidateSolution(candidatePiece, board)
-    }yield c
+    } yield c
   }
 
-  /** *
+  /**
     *
     * @param candidatePiece possible piece to be placed
     * @param board          board in which it will be placed
-    * @return iterate over all possible positions and look for a position in which a new board configuration can be created.
-    *         If it's a solution add it to solutions list
+    * @return iterate over all possible positions and look for a position in which a new valid board configuration can be created.
     */
   private def findCandidateSolution(candidatePiece: ChessPiece, board: Board): Set[Board] = {
-    def findCandidateSolutionAux(usedPieces: Set[ChessPiece], solutions: Set[Board]): Set[Board] = {
-      if (usedPieces.size != board.numberOfPieces) {
-        (for {
-          r <- 1 to board.M
-          c <- 1 to board.N
-          p = ChessPieceUtils.createPiece(candidatePiece, r, c)
-          b = board.place(p)
-          if board.isSafe(p) && !solutions.contains(b)
-        } yield b).toSet
-      } else {
-        solutions
-      }
-    }
-    findCandidateSolutionAux(board.usedPieces, Set[Board]())
+    (for {
+      r <- 1 to board.M
+      c <- 1 to board.N
+      p = ChessPieceUtils.createPiece(candidatePiece, r, c)
+      b = board.place(p)
+      if board.isSafe(p)
+    } yield b).toSet
   }
-
 
 }
