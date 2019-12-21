a3d6ee4fb9907b8961bfcfb26f93df56c118d387
ignaciomosca <ignaciomosca@outlook.com>
2019-12-21 08:14:27 -0300

    Make the Main class ZIO-like

--- src/main/scala/scalac/Main.scala
+++ src/main/scala/scalac/Main.scala
@@ -4,50 +4,48 @@ import scala.io.StdIn
 import scala.util.Try
 import scalac.ChessUtils.timer
 import scalac.ChessUtils.getInput
+import zio._
+import zio.console._
+import java.io.IOException
 
 /**
   * Created by ignacio on 08/10/16.
   */
 object Main extends App {
 
-  println("Define M")
-  val M = getInput {
-    Try(StdIn.readInt())
-  }
-  println("Define N")
-  val N = getInput {
-    Try(StdIn.readInt())
+  private def inputNumber(number: String) = {
+    ZIO
+    .effect(number.toInt)
+    .refineToOrDie[IOException]
   }
 
-  println("How many kings are to be placed on the board?")
-  val kings = getInput {
-    Try(StdIn.readInt())
-  }
-  println("How many queens are to be placed on the board?")
-  val queens = getInput {
-    Try(StdIn.readInt())
-  }
-  println("How many bishops are to be placed on the board?")
-  val bishops = getInput {
-    Try(StdIn.readInt())
-  }
-  println("How many rooks are to be placed on the board?")
-  val rooks = getInput {
-    Try(StdIn.readInt())
-  }
-  println("How many knights are to be placed on the board?")
-  val knights = getInput {
-    Try(StdIn.readInt())
+  private def validateInput(userInput: String) = {
+    inputNumber(userInput)
+    .orElse(putStrLn("invalid input") *> inputNumber(userInput))
   }
 
-  val pieces = ChessUtils.createListOfPieces(kings, queens, bishops, rooks, knights)
-  val totalPieces = pieces.size
+  private def getInput(message: String):ZIO[Console, IOException, Int] = (for {
+    _ <- putStrLn(message)
+    input <- getStrLn
+    number <- inputNumber(input)
+  } yield number) orElse getInput(message)
 
-  val solutions = timer {
-    val board = Board(M, N, Set(), totalPieces)
-    ChessChallengeSolver.solution(board, pieces, Set(board))
-  }
-  println(s"Number of Solutions: ${solutions.size}")
+  def run(args: List[String]) = (for {
+    m <- getInput("Define M")
+    n <- getInput("Define N")
+    kings <- getInput("How many kings are to be placed on the board?")
+    queens <- getInput("How many queens are to be placed on the board?")
+    bishops <- getInput("How many bishops are to be placed on the board?")
+    rooks <- getInput("How many rooks are to be placed on the board?")
+    knights <- getInput("How many knights are to be placed on the board?")
+    pieces = ChessUtils.createListOfPieces(kings, queens, bishops, rooks, knights)
+    totalPieces = pieces.size
+    board = Board(m,n, Set(), totalPieces)
+    solutions = ChessChallengeSolver.solution(board, pieces, Set(board))
+    _ <- putStrLn(s"Number of Solutions: ${solutions.size}")
+  } yield ()).run *> IO.succeed(0)
+
+
 }
 
 
